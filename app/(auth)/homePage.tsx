7105575598c01844951e07e46672d1d71e1e303a
pythonforever2023 <najmitdin@hotmail.com>
2025-03-13 12:53:24 +0100

    feat:j'ajoute HomePage

---
+++ app/(auth)/homePage.tsx
@@ -0,0 +1,25 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function HomePage() {
+    return (
+        <View className='flex-1 items-center justify-center bg-white p-4'>
+            <Text className='text-2x1 font-bold mb-8'>MEOW</Text>
+
+            <TouchableOpacity className='bg-gray-200 px-6 py-3 rounded-lg mb-4 w-3/4'>
+                <Link href="/sign-up" className='text-black text-center'>Créer un compte</Link>
+            </TouchableOpacity>
+
+
+            <View className='flex-row items-center my-2 w-3/4'>
+                <View className='flex-1 h-[1px] bg-gray-300' />
+                <Text className='mx-2 text-gray-500'>ou</Text>
+                <View className='flex-1 h-[1px] bg-gray-300' />
+            </View>
+
+            <TouchableOpacity className='bg-gray-200 px-6 py-3 rounded-lg w-3/4'>
+                <Link href="/sign-in" className='text-black text-center'>Se connecter</Link>
+            </TouchableOpacity>
+        </View>
+    )
+}
