4c94dbb3edb4d78d1f5d9b3587357c0fe3ea4dd4
hugobordier <hugo.bordier2003@gmail.com>
2025-03-27 13:14:10 +0100

    add better error message from the back

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -12,24 +12,12 @@ export const register = async (user: User) => {
       firstName: user.firstName,
       age: user.age,
       birthDate: user.birthDate,
-      city: user.city,
-      country: user.country,
-      gender: user.gender,
-      profilePicture: user.profilePicture,
-      bio: user.bio,
-      bankInfo: user.bankInfo,
-      rating: user.rating,
       phoneNumber: user.phoneNumber,
-      address: user.address,
-      identityDocument: user.identityDocument,
-      insuranceCertificate: user.insuranceCertificate,
-      isAdmin: user.isAdmin,
     });
     return response.data;
-  } catch (error) {
-    console.log(error);
+  } catch (error: any) {
     //@ts-ignore
-    throw error.response.data.error || "Echec de la connexion";
+    throw error.response.data.message || "Echec de la connexion";
   }
 };
 
@@ -45,7 +33,7 @@ export const login = async (form: { email: string; password: string }) => {
     const userData = await api.get("/authRoutes/me");
     return userData.data;
   } catch (error: any) {
-    throw error.response?.data?.error || "Échec de la connexion";
+    throw error.response.data.message || "Échec de la connexion";
   }
 };
 
@@ -53,7 +41,7 @@ export const forgotPassword = async (email: string) => {
   try {
     const { data } = await api.post("/authRoutes/forgot-password", { email });
   } catch (error: any) {
-    throw error?.response?.data.error || "Le code n'a pas pu etre envoyé";
+    throw error || "Le code n'a pas pu etre envoyé";
   }
 };
 
