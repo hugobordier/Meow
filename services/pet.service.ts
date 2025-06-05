ea1b21f558e13cd0bd2e9c90d031ca18df614c7e
Hippolyte <hippolyte.texier--bourrouilh@epfedu.fr>
2025-06-05 15:05:18 +0200

    ajouté tous les services et les création de photo il y en a des trucs là dedans

--- services/pet.service.ts
+++ services/pet.service.ts
@@ -14,7 +14,7 @@ export const createPet =async (data:Partial<Pet>)=>{
   }
 }
 
-export const getPetSitters = async (
+export const getPetsForAUser = async (
   filters?: PetQueryParams | null,
   pagination?: PaginationParams | null
 ): Promise<ApiResponsePet> => {
@@ -40,5 +40,125 @@ export const getPetSitters = async (
       error.response?.data?.message ??
       "Erreur lors de la récupération des pets"
     );
+  }
+};
+
+export const getPetById = async (petID: string) => {
+  try {
+    const response = await api.get(`/PetsRoutes/${petID}`);
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors de la récupération du pet:", error);
+    throw (
+      error.response?.data || { message: "Une erreur inconnue est survenue" }
+    );
+  }
+};
+
+
+const forbiddenFields = [
+  "photo_url",
+];
+
+export const updatePet = async (userID:string,data: Partial<Pet>) => {
+  try {
+    const sanitizedData = Object.keys(data).reduce((acc, key) => {
+      if (!forbiddenFields.includes(key)) {
+        (acc as any)[key] = (data as any)[key];
+      }
+      return acc;
+    }, {} as Partial<Pet>);
+    console.log("Sanitized data:", sanitizedData);
+
+    const response = await api.patch(`/PetsRoutes/${userID}`, sanitizedData);
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors du pet:", error);
+    throw (
+      error.response?.data || { message: "Une erreur inconnue est survenue" }
+    );
+  }
+};
+
+
+
+export const deletePet = async (petID:string) => {
+  try {
+    const response = await api.delete(`/PetsRoutes/pets/${petID}`);
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors de la suppression:", error);
+    throw error;
+  }
+};
+
+
+
+export const createPhotoprofilPet = async (id:string,image: string) => {
+  try {
+    const formData = new FormData();
+    const filename = image.split("/").pop() || "photo.jpg";
+    const match = /\.(\w+)$/.exec(filename);
+    const type = match ? `image/${match[1]}` : "image/jpeg";
+
+    formData.append("file", {
+      uri: image,
+      name: filename,
+      type,
+    } as any);
+
+    const response = await api.post(`/PetsRoutes/PhotoProfil/${id}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors de l'upload:", error);
+    throw error;
+  }
+};
+
+export const deletePhotoprofilPet = async (id:string) => {
+  try {
+    const response = await api.delete(`/PetsRoutes/PhotoProfil/${id}`);
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors de la suppression:", error);
+    throw error;
+  }
+};
+export const createPetImage = async (imageId:string,image: string) => {
+  try {
+    const formData = new FormData();
+    const filename = image.split("/").pop() || "photo.jpg";
+    const match = /\.(\w+)$/.exec(filename);
+    const type = match ? `image/${match[1]}` : "image/jpeg";
+
+    formData.append("file", {
+      uri: image,
+      name: filename,
+      type,
+    } as any);
+
+    const response = await api.post(`/PetImage/${imageId}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors de l'upload:", error);
+    throw error;
+  }
+};
+
+export const deletePetImage = async (imageId:string) => {
+  try {
+    const response = await api.delete(`/PetImage/${imageId}`);
+    return response.data;
+  } catch (error: any) {
+    console.error("Erreur lors de la suppression:", error);
+    throw error;
   }
 };
